bd451b6e05ee9efc4fdf957c519c895ab99ebf6f
Thomas Schulze <tschulze@codemonauts.com>
2018-10-24 16:17:41 +0200

    set nvm directory

--- includes/run.sh
+++ includes/run.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-export NVM_DIR="${XDG_CONFIG_HOME/:-$HOME/.}nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+. ~/.nvm/nvm.sh
 service php7.2-fpm start
 cd /local
 composer install
